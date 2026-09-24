3c781b50a1a8352ef74b9fa54b5421aee5f8b910
wednesday <95702954+presson8@users.noreply.github.com>
2026-09-24 14:28:20 +0800

    refactor: render local markdown in page detail

--- src/routes/Detail/PageDetail/index.tsx
+++ src/routes/Detail/PageDetail/index.tsx
@@ -1,7 +1,8 @@
 import React from "react"
 import styled from "@emotion/styled"
-import NotionRenderer from "../components/NotionRenderer"
+import MarkdownRenderer from "src/components/MarkdownRenderer"
 import usePostQuery from "src/hooks/usePostQuery"
+
 type Props = {}
 
 const PageDetail: React.FC<Props> = () => {
@@ -10,7 +11,7 @@ const PageDetail: React.FC<Props> = () => {
   if (!data) return null
   return (
     <StyledWrapper>
-      <NotionRenderer recordMap={data.recordMap} />
+      <MarkdownRenderer content={data.content || ""} />
     </StyledWrapper>
   )
 }
