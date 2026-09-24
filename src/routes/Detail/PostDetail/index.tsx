98b1295a49981f7d6a1346c43038448b84bb9fbc
wednesday <95702954+presson8@users.noreply.github.com>
2026-09-24 14:28:19 +0800

    refactor: render local markdown in post detail

--- src/routes/Detail/PostDetail/index.tsx
+++ src/routes/Detail/PostDetail/index.tsx
@@ -4,7 +4,7 @@ import Footer from "./PostFooter"
 import CommentBox from "./CommentBox"
 import Category from "src/components/Category"
 import styled from "@emotion/styled"
-import NotionRenderer from "../components/NotionRenderer"
+import MarkdownRenderer from "src/components/MarkdownRenderer"
 import usePostQuery from "src/hooks/usePostQuery"
 
 type Props = {}
@@ -27,9 +27,7 @@ const PostDetail: React.FC<Props> = () => {
           </div>
         )}
         {data.type[0] === "Post" && <PostHeader data={data} />}
-        <div>
-          <NotionRenderer recordMap={data.recordMap} />
-        </div>
+        <MarkdownRenderer content={data.content || ""} />
         {data.type[0] === "Post" && (
           <>
             <Footer />
@@ -50,8 +48,8 @@ const StyledWrapper = styled.div`
   padding-bottom: 3rem;
   border-radius: 1.5rem;
   max-width: 56rem;
-  background-color: ${({ theme }) =>
-    theme.scheme === "light" ? "white" : theme.colors.gray4};
+  background-color: ({ theme }) =>
+    theme.scheme === "light" ? "white" : theme.colors.gray4;
   box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
     0 2px 4px -1px rgba(0, 0, 0, 0.06);
   margin: 0 auto;
